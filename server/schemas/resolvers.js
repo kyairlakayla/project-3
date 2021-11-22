ddf153dba927c220bfa0594ab4bae6547a9681c6
aclar <a.clarke0307@gmail.com>
2021-11-22 18:54:34 -0500

    add login function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { User, Comments, Likes } = require('../models');
-//const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query:  {
@@ -31,8 +31,20 @@ const resolvers = {
           
             return user;
         },
-        login: async () => {
-    
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            return user;
         }
       }  
 }
