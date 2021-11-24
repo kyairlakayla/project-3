399b35cbd545d9bdac2f805e9b980185693cc8f5
Shane Bramble-Wade <sbramwade@msn.com>
2021-11-23 20:15:44 -0600

    implemented the single page recipe information

--- client/src/components/Single/index.js
+++ client/src/components/Single/index.js
@@ -1,32 +1,30 @@
 import React from "react";
-import { Card, Container, Message } from "semantic-ui-react";
+import { Header, Container } from "semantic-ui-react";
 
 function Single(props) {
-  console.log("single +++", props);
+  //  const ingredients =  props.detail.extendedIngredients;
   return (
     <>
-      {props && props.detail && (
-        <section id="single" className="single">
-          <h3>{props.detail.title}</h3>
-          <div>
-            <ul>
-              <li>Salt</li>
-              <li>Butter</li>
-              <li>Eggs</li>
-              <li>Flour</li>
-              <li>Vanilla</li>
-            </ul>
-          </div>
-          <div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tellus velit. Proin luctus erat ut pretium finibus. Donec quis lobortis magna. Phasellus pulvinar felis a dolor posuere pulvinar. Praesent quis augue in odio cursus commodo sed ac velit. In sollicitudin leo ligula, id ornare mauris ultrices ut. Maecenas a sem quam. Vestibulum commodo ante tincidunt semper porttitor. Vestibulum nec leo sit amet augue laoreet varius non ut odio. Nulla in commodo sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas maximus massa eu velit vulputate pharetra. Praesent ac commodo eros, et suscipit felis. Ut pellentesque lacinia orci ac efficitur. Duis ex mi, elementum sit amet mauris ac, convallis consequat tortor.
-            </p>
-          </div>
-          <div>
-            Comments
-          </div>
-        </section>
-      )}
+      <Container>
+        {props && props.detail && (
+          <section id="single" className="single">
+            <Header size="large" textAlign="center">
+              {props.detail.title}
+            </Header>
+            <div>
+              <ul>
+                {props.detail.extendedIngredients.map((i) => (
+                  <li key={i.id}>{i.originalString}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <p>{props.detail.instructions}</p>
+            </div>
+            <div>Comments</div>
+          </section>
+        )}
+      </Container>
     </>
   );
 }
